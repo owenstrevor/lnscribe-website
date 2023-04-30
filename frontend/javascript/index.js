46385e78b9091ef7f4cf55c62607b450e7c902d8
Trevor Owens <me@owenstrevor.com>
2023-04-30 15:09:54 -0500

    first commit

---
+++ frontend/javascript/index.js
@@ -0,0 +1,5 @@
+import "index.sass"
+import Bulmatown from "bulmatown"
+
+// Import all JavaScript & CSS files from src/_components
+import components from "bridgetownComponents/**/*.{js,jsx,js.rb,css}"
